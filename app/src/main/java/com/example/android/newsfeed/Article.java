f3852c99f93818773c3d8dbda07768fb87dcb790
Credo-Mbikayi <credo.mbikayi@umontreal.ca>
2020-05-11 17:23:59 -0400

    Implement the Parcelable interface into the Article Class

--- app/src/main/java/com/example/android/newsfeed/Article.java
+++ app/src/main/java/com/example/android/newsfeed/Article.java
@@ -1,6 +1,9 @@
 package com.example.android.newsfeed;
 
-public class Article {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Article implements Parcelable {
 
     private String mTitle;
     private String mTrailText;
@@ -19,6 +22,54 @@ public class Article {
         mFullArticleUrl = fullArticleUrl;
         mTimePublished = timePublished;
     }
+
+    /** Private constructor to be used
+     *  by the createFromParcel method of
+     *  Parcelable Creator. Will create an Article object from the
+     *  Parcel*/
+
+    private Article (Parcel in) {
+
+        this( in.readString(), // The title
+                in.readString(), // The TrailText
+                in.readString(), // The ThumbnailUrl Url
+                in.readString(), // The Full Article Url
+                in.readString()); // The Publication Time
+
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /**Will be used to turn The Article Object into a parcel.*/
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+
+        parcel.writeString(mTitle); // Title
+        parcel.writeString(mTrailText); // The Trail Text
+        parcel.writeString(mThumbnailUrl); // The Thumbnail Url
+        parcel.writeString(mFullArticleUrl); // The Url of the Full Article
+        parcel.writeString(mTimePublished); // The Publication time
+    }
+
+    /** Will help us generate an instance of
+     *  "AMovie" from a Parcel,
+     *  by using the constructor "Article(Parcel in)". */
+
+    public final Parcelable.Creator<Article> CREATOR = new Parcelable.Creator<Article>() {
+        @Override
+        public Article createFromParcel(Parcel parcel) {
+            return new Article(parcel);
+        }
+
+        @Override
+        public Article[] newArray(int i) {
+            return new Article[i];
+        }
+
+    };
 
     // Getters of the class.
     // They are used inside the Loader
